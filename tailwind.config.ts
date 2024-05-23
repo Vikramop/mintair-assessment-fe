c1f3f3b00bff86c70fdac35de72a48cf2ffc4e22
Vikramop <veervikramsingh132@gmail.com>
2024-05-23 14:00:46 +0530

    Connecting to wallet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '0.7rem',
+          sm: '0.7rem',
+          lg: '1rem',
+          xl: '1rem',
+          '2xl': '6rem',
+        },
       },
     },
   },
